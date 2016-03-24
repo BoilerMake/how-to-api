c54f985791af4ebcea2e6dc0599953ff7c7623c4
Jacob Brabec <jbrabec@purdue.edu>
2016-03-23 23:57:21 -0400

    Cleaned up and commented code

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,11 +1,18 @@
 var app = angular.module('movies', []);
 var API = "http://localhost:8080/api/"; //Your server url goes here
 
-
+/**
+ * Here we define a new controller called MoviesController. 
+ * We require $http, so angular injects it automatically. 
+ */
 app.controller("MoviesController", ['$http', function($http) {
 
-	var collection = this;
-
+	var collection = this; //Save this controller 
+	/**
+	 * Use the $http method to send a GET request to our movie API. 
+	 * We provide some details in the first paramater. 
+	 * We use the .then() method to pass in our success and error functions. 
+	 */
 	$http({
 	 method: 'GET',
 	 url: API+'movies',
@@ -13,7 +20,11 @@ app.controller("MoviesController", ['$http', function($http) {
 	   'Content-Type': undefined
 	 }
 	}).then(function success(response) {
-		collection.movies = response.data;
+		
+		//If this was successfull, handle the data from the server
+		collection.movies = response.data; //Copy the response data into our movie array
+
+		//Load the image for each movie
 		angular.forEach(collection.movies, function(movie) {
 			fetchMovieImage(movie);
 		})
@@ -26,6 +37,11 @@ app.controller("MoviesController", ['$http', function($http) {
 	 * Adds a new movie to the array.
 	 */
 	collection.addNewMovie = function() {
+		/**
+		 * Like before, we use the $http method to send a POST request to our movie API. 
+		 * Because its a POST request we provide the data we want to send.
+		 * We use the .then() method to pass in our success and error functions. 
+		 */
 		$http({
 			method: 'POST',
 			url: API+'movies',
@@ -34,16 +50,18 @@ app.controller("MoviesController", ['$http', function($http) {
 				description: collection.newMovie.description
 			}
 		}).then(function successCallback(response) {
+			//Make sure the response has the data we need
 			if(response.data.title != null && response.data.description != null) {
-				console.log(JSON.stringify(response.data));
-				collection.newMovie = {};
-			    collection.movies.push(response.data);
-			    collection.expandNewMovieEntry = false;
-			    fetchMovieImage(response.data);
+				//Log the data just for reference
+				console.log(JSON.stringify(response.data)); //We use JSON.stringify to make the response data human-readable
+				collection.newMovie = {}; //Clear the newMovie data to reset the form. 
+			    collection.movies.push(response.data); //Add the new movie to our movie array
+			    collection.expandNewMovieEntry = false; //Close the movie creation form
+			    fetchMovieImage(response.data); //Load the image for this new movie
 			} else {
-				console.log("Failed: "+JSON.stringify(response.data));
+				//The response was missing data, something went wrong. 
+				console.log("Failed: "+JSON.stringify(response.data)); //We use JSON.stringify to make the response data human-readable
 			}
-
 		}, function errorCallback(error) {
 		    console.log(error);
 		});
@@ -53,6 +71,11 @@ app.controller("MoviesController", ['$http', function($http) {
 	 * Adds a new review to the array
 	 */
 	collection.addReview = function(movie) {
+		/**
+		 * Again, we use the $http method to send a POST request to our movie API. 
+		 * Because its a POST request we provide the data we want to send.
+		 * We use the .then() method to pass in our success and error functions. 
+		 */
 		$http({
 			method: 'POST',
 			url: API+'movies/' + movie._id + '/reviews',
@@ -61,15 +84,15 @@ app.controller("MoviesController", ['$http', function($http) {
 				body: movie.review.body
 			}
 		}).then(function successCallback(response) {
+			//Make sure the response data is okay
 			if(response.data.score != null && response.data.body != null) {
-				movie.review = {};
-		    	movie.reviews.push(response.data);
+				movie.review = {}; //Clear the review data to reset the form
+		    	movie.reviews.push(response.data); //Add this review to this movie's array
 			} else {
-				console.log("Failed: "+JSON.stringify(response.data));
+				console.log("Failed: "+JSON.stringify(response.data)); //We use JSON.stringify to make the response data human-readable
 			}
-
 		}, function errorCallback(error) {
-		    console.log(error);
+		    console.log(error); //Something was wrong with our request, print the error
 		});
 	};
 
@@ -77,6 +100,11 @@ app.controller("MoviesController", ['$http', function($http) {
 	 * Fetches the image for the given movie and updates data.
 	 */
 	function fetchMovieImage(movie) {
+		/**
+		 * We use the $http method to send a GET request to a movie poster API. 
+		 * We add the movie name into the middle of the url so the API knows what to search for.
+		 * We use the .then() method to pass in our success and error functions. 
+		 */
 		$http({
 		 method: 'GET',
 		 url: 'http://www.omdbapi.com/?t='+movie.title+'&y=&plot=short&r=json',
@@ -84,10 +112,11 @@ app.controller("MoviesController", ['$http', function($http) {
 		   'Content-Type': undefined
 		 }
 		}).then(function(response){
+			//If the response data was valid, set the movie image
 			if(response.data.Poster != null)
-				movie.image = response.data.Poster;
+				movie.image = response.data.Poster; //Set the movie image to the url we got from the server
 		},function(error){
-	    	console.log(error);
+	    	console.log(error); //Something went wrong, log the error
 		})
 	}
 }]);
