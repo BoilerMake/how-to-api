ebf0af7b7141894166b68675502feff7344db6f8
Jacob Brabec <jbrabec@purdue.edu>
2016-03-31 22:11:37 -0400

    REALLY fixed it this time

--- runMongod.sh
+++ runMongod.sh
@@ -1,10 +1,5 @@
-#Sets up mongod. Only needs to be run once. 
-#Run this file with ./setupMongod.sh
+#Run this file with ./runMongod.sh
 #nojournol because the journal file is too big
 #bindip because we cant bind to 0.0.0.0
 #rest runs it on port 28017
-echo "Setting up mongod..."
-mkdir data
-echo 'mongod --bind_ip=$IP --dbpath=data --nojournal --rest "$@"' > mongod
-chmod a+x mongod
-echo "Mongod is ready! Run 'mongod' to start it!"
+mongod --bind_ip=$IP --dbpath=data --nojournal --rest "$@"
